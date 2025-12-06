3b6461e311fedb588baeff1f114b02ccac5c0d2c
Nasteha153 <nasteha351@gmail.com>
2025-12-06 22:36:39 +0300

    uploaded exercise4

---
+++ Exercise4/main.js
@@ -0,0 +1,19 @@
+// function add(x,y){
+//     return x + y;
+// }
+// console.log(add(5,10));
+// console.log(add(10,90));
+// console.log(add(100,200));
+// console.log(add(50,50))
+
+let add=function(x,y){
+    return x + y;
+}
+let result=add(5,10);
+console.log(result);
+result=add(10,90);
+console.log(result);
+result=add(100,200);
+console.log(result);
+result=add(50,50)
+console.log(result);
