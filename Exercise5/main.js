4e321fcdf0ac2974212ed626ba2f501f754fdc8d
Nasteha153 <nasteha351@gmail.com>
2025-12-08 16:35:46 +0300

    uploaded exercise5

---
+++ Exercise5/main.js
@@ -0,0 +1,12 @@
+const add=(x,y)=>{
+    return x+y;
+}
+let result=add(20,20)
+console.log(result)
+result=add(100,200)
+console.log(result)
+result=add(5,10)
+console.log(result)
+result=add(40,70)
+console.log(result)
+
