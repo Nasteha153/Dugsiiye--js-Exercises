aa6b70cc7c679891dcd35c47f01d76c743829cb4
Nasteha153 <nasteha351@gmail.com>
2025-12-15 09:42:21 +0300

    added exercise 10

---
+++ Exercise10/main.js
@@ -0,0 +1,20 @@
+let library=[
+    {
+        title: 'The Road Ahead',
+        author: 'Bill Gates',
+        year: 1995,
+    },
+    {
+        title : 'Pride and Prejudice',
+        author: 'Jane Austen',
+        year: 1813,
+    },
+    {
+        title: 'Harry Potter series',
+        author: 'J.K. Rowling',
+        year: 1997,
+    }
+]
+console.log(library);
+console.log(library[0].title);
+console.log(library[1].author);
