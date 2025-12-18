709f3aa6d827df58ae8c4b2d470ebfc1626c7fbe
Nasteha153 <nasteha351@gmail.com>
2025-12-18 18:21:41 +0300

    added exercise 17

---
+++ Exercise17/main.js
@@ -0,0 +1,17 @@
+const temperature =10;
+if(temperature <0)
+{
+ console.log("vey cold");
+}
+else if(temperature >=0 &&  temperature < 16)
+{
+ console.log("Cold");
+}
+else if(temperature>=15 && temperature <= 25)
+{
+    console.log("Warm");
+}
+else
+    {
+    console.log('Hot');
+}
